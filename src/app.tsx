49649150c69822b93359dd4539c1eccde96e3c0f
Jon Ascencio <jonascencio1@gmail.com>
2020-11-17 14:15:42 -0800

    updated boilerplate. Webpack and hot reload working

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+
+const App = () => {
+  return (
+    <h1>
+     lets get it
+    </h1>
+  )
+}
+ReactDom.render(<App />,document.getElementById('root'));
+
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-
-
-ReactDom.render(
-
-    <div>hello</div>,
- 
-document.getElementById('root'));
