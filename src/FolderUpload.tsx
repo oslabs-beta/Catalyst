9764cc3ed2d8a5e7b1fa215481077de98313b206
Danny <chungdanny63@yahoo.com>
2020-11-18 10:58:42 -0800

    completed file tree

--- src/FolderUpload.tsx
+++ src/FolderUpload.tsx
@@ -1,7 +1,51 @@
 import * as React from 'react'
 import * as electron from 'electron'
 import {remote} from 'electron'
+import fs from 'fs'
 
+
+// allows electron to use node module fs 
+const electronFs = remote.require('fs')
+
+class FileTree {
+    filepath: string
+    name: string
+    children: FileTree[]
+
+
+    constructor(filepath: string, name: string){
+        this.filepath = filepath
+        this.name = name
+        this.children = []
+
+    }
+
+    // directory will be root directory path
+    createTree(directory: string): FileTree[]{
+        let treeElements: FileTree[] = []
+
+        // readdirSync will return all the names of the files in the directory as an array 
+        electronFs.readdirSync(directory).forEach((fileName: string) =>{
+
+            if(fileName !== ".git" && fileName !== "node_modules" && fileName !== "dist"){
+                let fileInfo = new FileTree(directory + "/" + fileName, fileName)
+
+                // statsync will return an object with properties of the filepath indicated
+                let moreFileInfo = electronFs.statSync(fileInfo.filepath)
+                
+                // if the file that is currently at is a directory it will append the children to it
+                if(moreFileInfo.isDirectory()){
+                    fileInfo.children = fileInfo.createTree(fileInfo.filepath)
+                }
+
+                treeElements.push(fileInfo)
+            }
+
+        })
+        return treeElements
+    }
+
+}
 
 export const FolderUpload: React.FC = () => {
 
@@ -19,13 +63,23 @@ export const FolderUpload: React.FC = () => {
             message: 'please choose a project'
         })
 
+        console.log(Project)
         // if the user cancels the action then undefined will be returned
         // if the user successfully completes the action then a string array will be returned
          // Project is an object that holds canceled (boolean to check if it was cancelled) and filePaths (array of filepaths)
         if(Project && !Project.canceled){
             // holds the directory of the project that was selected 
             let projectDirectory = Project.filePaths[0]
-            console.log(projectDirectory)
+
+            // use regex to find all \ in the case of a windows user and replace with /
+            projectDirectory = projectDirectory.replace(/\\/g, '/')
+
+            // will create a new FileTree object for the root directory
+            const rootTree = new FileTree(projectDirectory, "root")
+
+            // will return an array of FileTree objects along with any children associated with it 
+            const fileTree = rootTree.createTree(projectDirectory)
+            console.log(fileTree)
         }
     }
 
