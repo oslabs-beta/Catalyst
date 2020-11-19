31a6f41bcf8e25323d4365ecc08e1dfa6e251bd7
Jon Ascencio <jonascencio1@gmail.com>
2020-11-19 08:54:02 -0800

    redux file structure re-arranged, file-upload selectors refined

---
+++ src/reduxComponents/actions/actions.ts
@@ -0,0 +1,21 @@
+// import * from '../constants/actionTypes';
+
+
+/* interface IncrementAction {
+  type: typeof Increment_COUNTER
+};
+
+interface DecrementAction {
+  type: typeof DECREMENT_COUNTER
+};
+
+export type ActionTypes = IncrementAction | DecrementAction
+ */
+
+ 
+/* // const SetAge = (payload: number) => {
+  return {
+    type: bindAction.SET_AGE,
+    payload 
+  }
+} */
