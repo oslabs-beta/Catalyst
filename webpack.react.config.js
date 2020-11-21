dd1d1c05787e23515d964c853b63b914f57d97cc
Gavin Crews <gavincrews@Gavins-Air.hsd1.ca.comcast.net>
2020-11-21 14:14:41 -0800

    frontpage styling

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -25,6 +25,14 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       }
     ],
   },
