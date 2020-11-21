c05d1ad3e388144e991f3f34591ff04aa8aec952
Jon Ascencio <jonascencio1@gmail.com>
2020-11-20 19:37:12 -0800

    scss resolved

--- src/components/FileViewer.tsx
+++ src/components/FileViewer.tsx
@@ -27,10 +27,7 @@ export const FileViewer: React.FC = () =>{
       </div>
       
     })
-    console.log(counter)
-    console.log(x)
     return tester
-    // fs.writeFileSync('C:/Users/Danny/Documents/Codesmith/Projects/Catalyst/tester.txt', readMe)
   }
   
 
