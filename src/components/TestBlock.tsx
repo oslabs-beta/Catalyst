52435443a2e01ad7f94d370ec7e179eec2465f10
Jon Ascencio <jonascencio1@gmail.com>
2020-11-24 20:19:37 -0800

    merging with master

--- src/components/TestBlock.tsx
+++ src/components/TestBlock.tsx
@@ -34,12 +34,17 @@ export const TestBlock: React.FC = () => {
 
 
   const handleClick = () => {
-    let test = `describe('${describeInputGlobal[0]}', () => {
-      it('${itInputGlobal[0]}', () => {
-        expect(wrapper${expectGlobal[0].firstInput0}()${expectGlobal[0].testTypes}(${expectGlobal[0].lastInput0})
-      })
-    });`
-    console.log(test)
+    // let test = `describe('${describeInputGlobal[0]}', () => {
+    //   it('${itInputGlobal[0]}', () => {
+    //     expect(wrapper${expectGlobal[0].firstInput0}()${expectGlobal[0].testTypes}(${expectGlobal[0].lastInput0})
+    //   })
+    // });`
+    // console.log(test)
+
+
+
+
+
     // console.log('i was clicked');
     // console.log('expect(wrapper.text()).toEqual("userinput")');
     // console.log('expect(wrapper.find("").toEqual("userinput")');
@@ -58,11 +63,50 @@ export const TestBlock: React.FC = () => {
     // console.log('expect(wrapper.text().toMatch("userInput")');
     // console.log('expect(wrapper.find("userInput").toHaveLength("userInput")');
     
+    const globalData = {describeGlobal, describeInputGlobal, itsGlobal, itInputGlobal, expectGlobal};
+    console.log('this is all global data', globalData);
     console.log('this is describeGlobal', describeGlobal);
     console.log('this is its global',itsGlobal );
     console.log('this is expects global', expectGlobal);
     console.log('this is describe input global', describeInputGlobal);
     console.log('this is the it input global', itInputGlobal);
+
+    const keysOfDescribe = Object.values(describeGlobal);
+    console.log('these are k describe', keysOfDescribe);
+    const keysOfIts = Object.values(itsGlobal);
+    console.log('these are k of its', keysOfIts);
+    const keysOfExpects = Object.values(expectGlobal);
+    console.log('these are k of expects',keysOfExpects);
+    const keysOfDescribeInputs = Object.values(describeInputGlobal);
+    console.log('these are k of DI', keysOfDescribeInputs);
+    const keysOfItInputs = Object.values(itInputGlobal);
+    console.log('these are k of II', keysOfItInputs);
+
+    for (const itIndex of keysOfDescribe){
+      for (const itStatements in itIndex) {
+        console.log('this is hopefully the expects', expectGlobal[itStatements]);
+        
+      }
+    }
+
+
+    // let expectObj = {}
+    // for (let i = 0; i < Object.keys(expectGlobal).length; i+=1) {
+    //   expectObj[`${i}`] = `expect(wrapper${expectGlobal[i][`firstInput${i}`]}()${expectGlobal[i].testTypes}(${expectGlobal[i][`lastInput${i}`]})`;
+    // }
+    // console.log(expectObj)
+
+  //   const iterateThrough = (obj: any) => {
+  //   Object.keys(obj).forEach((key) => {
+  //     if (typeof obj[key] === 'object') {
+  //       return iterateThrough(obj[key]);
+  //     }
+  //     console.log(`this is ${key} = ${obj[key]}`);
+  //   })
+  // }
+
+
+    // iterateThrough(globalData);
   }
 
 
